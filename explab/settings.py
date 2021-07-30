3474bdebc239f8325cb6ee9693b43b80a16b70bf
DineshRayala <66214083+DineshRayala@users.noreply.github.com>
2021-07-30 19:33:23 +0530

    Final Changes

--- explab/settings.py
+++ explab/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'tc%p3z*=t+mk=wvvb&^cv+a3z)=#%z##ct!vxd+@c4z-b6x*ga'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','meetup-chatapplication.herokuapp.com']
 
 
 # Application definition
