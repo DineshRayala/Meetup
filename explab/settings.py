1152f3b85ddd24349e35bcaf43900aafbbe666e3
DineshRayala <66214083+DineshRayala@users.noreply.github.com>
2021-07-30 20:13:34 +0530

    Update settings.py

--- explab/settings.py
+++ explab/settings.py
@@ -129,7 +129,7 @@ if DEBUG:
    os.path.join(BASE_DIR, 'static'),
    ]
 else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+   STATIC_ROOT = os.path.join(BASE_DIR,'meetup/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
